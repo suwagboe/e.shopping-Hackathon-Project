f825f47175f93bc4a91d721ced857ea4bfa2f9c5
Lilia Yudina <liliayudina@gmail.com>
2020-04-21 16:33:58 -0400

    created watchListController and Cell

---
+++ e.shopping-Hackathon/Views/WatchListViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListViewCell.swift
+//  e.shopping-Hackathon
+//
+//  Created by Lilia Yudina on 4/21/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class WatchListViewCell: UITableViewCell {
+    
+    
+    public func configureCell() {
+        
+    }
+}
