fbbdd99ede6689fe80a2b05b3f12ac39b902b134
uwagboe <sm.uwagboe@gmail.com>
2020-04-21 16:54:38 -0400

    worked on the cell for the search collection view, and the view of the collection view

--- e.shopping-Hackathon/Controllers/SearchController.swift
+++ e.shopping-Hackathon/Controllers/SearchController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class SearchController: UIViewController {
+    
+    private let searchController = SearchView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        configureController()
+      
     }
     
-
+    private func configureController(){
+        view = searchController
+       searchController.collection.register(UINib(nibName: "SearchCell", bundle: nil), forCellWithReuseIdentifier: "searchCell")
+         
+         
+        searchController.searchBar.delegate = self
+        searchController.collection.delegate = self
+        searchController.collection.dataSource = self
+    }
+    
   
+}
+
+extension SearchController: UISearchBarDelegate {
+    
+}
+
+extension SearchController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "searchCell", for: indexPath) as? SearchCell else {
+            fatalError("Could not cast as SearchCell double check if this was applied correctly.")
+        }
+
+        return cell
+        
+    }
+    
+    
+}
+
+extension SearchController: UICollectionViewDelegateFlowLayout {
+    
+    
+    
 }
