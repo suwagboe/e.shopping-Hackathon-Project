8fba701439d6c357031ae5033ff594d778ab8809
Gregory Keeley <gregorykeeley@pursuit.org>
2020-04-22 20:09:11 -0400

    Added basic detail view UI elements

--- e.shopping-Hackathon/Views/DetailView.swift
+++ e.shopping-Hackathon/Views/DetailView.swift
@@ -9,7 +9,55 @@
 import UIKit
 
 class DetailView: UIView {
-
- 
-
+    
+    private lazy var companyLogoImageView: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    private lazy var companyNameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private lazy var descriptionTextView: UITextView = {
+        let field = UITextView()
+        return field
+    }()
+    private lazy var transparentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private lazy var scoreLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private lazy var scoreImage: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    private lazy var contextButton: UIButton =  {
+        let button = UIButton()
+        return button
+    }()
+    // This will be set behind the alert
+    private lazy var alertView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private lazy var newsArticlesButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    private func commonInit() {
+        
+    }
+    
 }
